927e835d0eaef4db1e83c51492f385cf42c1dabf
Kinsey Durham <kinseyann505@gmail.com>
2013-10-09 09:44:30 -0600

    Anagrams printed out correctly on the screen

--- word_parser.rb
+++ word_parser.rb
@@ -5,7 +5,36 @@ class WordParser
     @word = word
   end
 
-  def sorted_words
-    word.chars.sort
+  def printed_anagrams
+    individual_anagrams.map do |individual_anagram|
+      puts individual_anagram
+    end
   end
+
+  private
+
+  def sorted_words
+    words.group_by do |word|
+      word.chars.sort
+    end
+  end
+
+  def grouped_words
+    sorted_words.values
+  end
+
+  def anagrams
+    grouped_words.keep_if do |grouped_word|
+      grouped_word.length >= 2
+    end
+  end
+
+  def joined_anagrams
+    anagrams.join(' ')
+  end
+
+  def individual_anagrams
+    joined_anagrams.split
+  end
+
 end
